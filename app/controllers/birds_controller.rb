1779903573f5948b8730c5b8ba3082d2c19da395
Christopher Perez <ChrisAmadeusPerez@gmail.com>
2022-12-07 04:24:01 -0500

    commit tree f2c092c48373c156b7da1aa562782b60f4ec2cc5

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,7 @@
 class BirdsController < ApplicationController
 
+  wrap_parameters format: []
+
   # GET /birds
   def index
     birds = Bird.all
@@ -21,5 +23,20 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+
+  # POST /birds
+  def create
+    bird = Bird.create(bird_params)
+    render json: bird, status: :created
+  end
+
+  private
+  # all methods below here are private
+
+  def bird_params
+    params.permit(:name, :species)
+  end
+
+  
 
 end
